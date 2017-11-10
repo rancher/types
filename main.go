e52763be7180067618e1530797da282cbbf9af04
Darren Shepherd <darren@rancher.com>
2017-11-10 13:23:15 -0700

    Initial Commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/rancher/types/io.cattle.management/v1"
+)
+
+var VERSION = "v0.0.0-dev"
+
+func main() {
+	fmt.Println("Nothing")
+}
---
+++ package/Dockerfile
@@ -0,0 +1,3 @@
+FROM ubuntu:16.04
+COPY types /usr/bin/
+CMD ["types"]
