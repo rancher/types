bef3d21dd7dd4ee708458bd6d32c5bbea0112f7b
Darren Shepherd <darren@rancher.com>
2017-11-10 22:09:22 -0700

    Update generated code

--- io.cattle.cluster/v1/zz_generated_deepcopy.go
+++ io.cattle.cluster/v1/zz_generated_deepcopy.go
@@ -5,10 +5,11 @@
 package v1
 
 import (
+	reflect "reflect"
+
 	core_v1 "k8s.io/api/core/v1"
 	conversion "k8s.io/apimachinery/pkg/conversion"
 	runtime "k8s.io/apimachinery/pkg/runtime"
-	reflect "reflect"
 )
 
 func init() {
