07dc303215902499037a26e8153fa390d4fdd462
Prachi Damle <prachi@rancher.com>
2017-11-16 10:56:53 -0800

    Initial types for token authentication

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	authzSchema "github.com/rancher/types/apis/authorization.cattle.io/v1/schema"
+	authnSchema "github.com/rancher/types/apis/authentication.cattle.io/v1/schema"
 	clusterSchema "github.com/rancher/types/apis/cluster.cattle.io/v1/schema"
 	workloadSchema "github.com/rancher/types/apis/workload.cattle.io/v1/schema"
 	"github.com/rancher/types/generator"
@@ -13,4 +14,5 @@ func main() {
 	generator.Generate(clusterSchema.Schemas)
 	generator.Generate(workloadSchema.Schemas)
 	generator.Generate(authzSchema.Schemas)
+	generator.Generate(authnSchema.Schemas)
 }
