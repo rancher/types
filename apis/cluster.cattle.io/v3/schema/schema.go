a6b44f25da4f2f4e99ce941eec5e4f247def3510
Darren Shepherd <darren@rancher.com>
2017-12-05 14:20:44 -0700

    Copy namespace to cluster schemas

--- apis/cluster.cattle.io/v3/schema/schema.go
+++ apis/cluster.cattle.io/v3/schema/schema.go
@@ -3,6 +3,7 @@ package schema
 import (
 	"github.com/rancher/norman/types"
 	m "github.com/rancher/norman/types/mapper"
+	"github.com/rancher/types/apis/project.cattle.io/v3/schema"
 	"github.com/rancher/types/factory"
 	"github.com/rancher/types/mapper"
 	"k8s.io/api/core/v1"
@@ -16,8 +17,22 @@ var (
 	}
 
 	Schemas = factory.Schemas(&Version).
-		Init(nodeTypes)
+		Init(nodeTypes).
+		Init(copyNamespace)
 )
+
+func copyNamespace(schemas *types.Schemas) *types.Schemas {
+	cloneProjectSchema("namespaceStatus", schemas)
+	cloneProjectSchema("namespace", schemas)
+	return schemas
+}
+
+func cloneProjectSchema(name string, schemas *types.Schemas) {
+	schema := schema.Schemas.Schema(&schema.Version, name)
+	copy := *schema
+	copy.Version = Version
+	schemas.AddSchema(&copy)
+}
 
 func nodeTypes(schemas *types.Schemas) *types.Schemas {
 	return schemas.
