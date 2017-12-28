7b843615898cc36881713df9ca4146d6d58ceaab
Darren Shepherd <darren@rancher.com>
2017-12-28 09:56:37 -0700

    Update vendor

--- vendor/github.com/rancher/norman/condition/condition.go
+++ vendor/github.com/rancher/norman/condition/condition.go
@@ -38,6 +38,27 @@ func (c Cond) IsUnknown(obj runtime.Object) bool {
 func (c Cond) Reason(obj runtime.Object, reason string) {
 	cond := findOrCreateCond(obj, string(c))
 	getFieldValue(cond, "Reason").SetString(reason)
+}
+
+func (c Cond) Message(obj runtime.Object, message string) {
+	cond := findOrCreateCond(obj, string(c))
+	v := getFieldValue(cond, "Message")
+	if v.IsValid() {
+		v.SetString(message)
+	}
+}
+
+func (c Cond) ReasonError(obj runtime.Object, err error) {
+	cond := findOrCreateCond(obj, string(c))
+	if ce, ok := err.(*conditionError); ok {
+		getFieldValue(cond, "Reason").SetString(ce.reason)
+		v := getFieldValue(cond, "Message")
+		if v.IsValid() {
+			v.SetString(ce.Error())
+		}
+	} else {
+		getFieldValue(cond, "Reason").SetString(err.Error())
+	}
 	touchTS(cond)
 }
 
@@ -65,14 +86,14 @@ func (c Cond) Once(obj runtime.Object, f func() (runtime.Object, error)) (runtim
 
 	if err != nil {
 		c.False(obj)
-		c.Reason(obj, err.Error())
+		c.ReasonError(obj, err)
 		return obj, err
 	}
 	c.True(obj)
 	return obj, nil
 }
 
-func (c Cond) Do(obj runtime.Object, f func() (runtime.Object, error)) error {
+func (c Cond) Do(obj runtime.Object, f func() (runtime.Object, error)) (runtime.Object, error) {
 	c.Unknown(obj)
 	newObj, err := f()
 	if newObj != nil {
@@ -81,11 +102,13 @@ func (c Cond) Do(obj runtime.Object, f func() (runtime.Object, error)) error {
 
 	if err != nil {
 		c.False(obj)
-		c.Reason(obj, err.Error())
-		return err
+		c.ReasonError(obj, err)
+		return obj, err
 	}
 	c.True(obj)
-	return nil
+	c.Reason(obj, "")
+	c.Message(obj, "")
+	return obj, nil
 }
 
 func touchTS(value reflect.Value) {
@@ -151,4 +174,20 @@ func getFieldValue(v reflect.Value, name ...string) reflect.Value {
 		return field
 	}
 	return getFieldValue(field, name[1:]...)
+}
+
+func Error(reason string, err error) error {
+	return &conditionError{
+		reason:  reason,
+		message: err.Error(),
+	}
+}
+
+type conditionError struct {
+	reason  string
+	message string
+}
+
+func (e *conditionError) Error() string {
+	return e.message
 }
