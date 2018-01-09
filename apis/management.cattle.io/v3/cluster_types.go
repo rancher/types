ac41e003124d4ef77404ebf476fe3775d91a2d4a
kinarashah <kinarahs@usc.edu>
2018-01-09 12:38:59 -0800

    ServiceAccountSecretName

--- apis/management.cattle.io/v3/cluster_types.go
+++ apis/management.cattle.io/v3/cluster_types.go
@@ -53,15 +53,14 @@ type ClusterStatus struct {
 	Conditions []ClusterCondition `json:"conditions,omitempty"`
 	//Component statuses will represent cluster's components (etcd/controller/scheduler) health
 	// https://kubernetes.io/docs/api-reference/v1.8/#componentstatus-v1-core
-	ComponentStatuses   []ClusterComponentStatus `json:"componentStatuses,omitempty"`
-	APIEndpoint         string                   `json:"apiEndpoint,omitempty"`
-	ServiceAccountToken string                   `json:"serviceAccountToken,omitempty"`
-	CACert              string                   `json:"caCert,omitempty"`
-	Capacity            v1.ResourceList          `json:"capacity,omitempty"`
-	Allocatable         v1.ResourceList          `json:"allocatable,omitempty"`
-	AppliedSpec         ClusterSpec              `json:"appliedSpec,omitempty"`
-	Requested           v1.ResourceList          `json:"requested,omitempty"`
-	Limits              v1.ResourceList          `json:"limits,omitempty"`
+	ComponentStatuses        []ClusterComponentStatus `json:"componentStatuses,omitempty"`
+	APIEndpoint              string                   `json:"apiEndpoint,omitempty"`
+	ServiceAccountSecretName string                   `json:"serviceAccountSecretName,omitempty"`
+	Capacity                 v1.ResourceList          `json:"capacity,omitempty"`
+	Allocatable              v1.ResourceList          `json:"allocatable,omitempty"`
+	AppliedSpec              ClusterSpec              `json:"appliedSpec,omitempty"`
+	Requested                v1.ResourceList          `json:"requested,omitempty"`
+	Limits                   v1.ResourceList          `json:"limits,omitempty"`
 }
 
 type ClusterComponentStatus struct {
