fd99a3deec95637b4404199f69f439a3be71ca5e
Darren Shepherd <darren@rancher.com>
2018-01-12 12:07:47 -0700

    Rename TemplateVersionID to ExternalID

--- apis/management.cattle.io/v3/stack_types.go
+++ apis/management.cattle.io/v3/stack_types.go
@@ -15,16 +15,16 @@ type Stack struct {
 }
 
 type StackSpec struct {
-	ProjectName       string            `json:"projectName,omitempty" norman:"type=reference[project]"`
-	Description       string            `json:"description,omitempty"`
-	InstallNamespace  string            `json:"installNamespace,omitempty"`
-	TemplateVersionID string            `json:"templateVersionId,omitempty"`
-	Templates         map[string]string `json:"templates,omitempty"`
-	Answers           map[string]string `json:"answers,omitempty"`
-	Prune             bool              `json:"prune,omitempty"`
-	Tag               map[string]string `json:"tag,omitempty"`
-	User              string            `json:"user,omitempty"`
-	Groups            []string          `json:"groups,omitempty"`
+	ProjectName      string            `json:"projectName,omitempty" norman:"type=reference[project]"`
+	Description      string            `json:"description,omitempty"`
+	InstallNamespace string            `json:"installNamespace,omitempty"`
+	ExternalID       string            `json:"externalId,omitempty"`
+	Templates        map[string]string `json:"templates,omitempty"`
+	Answers          map[string]string `json:"answers,omitempty"`
+	Prune            bool              `json:"prune,omitempty"`
+	Tag              map[string]string `json:"tag,omitempty"`
+	User             string            `json:"user,omitempty"`
+	Groups           []string          `json:"groups,omitempty"`
 }
 
 type StackStatus struct {
