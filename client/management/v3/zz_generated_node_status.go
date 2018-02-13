56a763a524acaad5597c2e8926e50c029bdede1b
Darren Shepherd <darren@rancher.com>
2018-02-13 11:02:25 -0700

    Update generated code

--- client/management/v3/zz_generated_node_status.go
+++ client/management/v3/zz_generated_node_status.go
@@ -4,10 +4,16 @@ const (
 	NodeStatusType                 = "nodeStatus"
 	NodeStatusFieldAllocatable     = "allocatable"
 	NodeStatusFieldCapacity        = "capacity"
+	NodeStatusFieldConditions      = "conditions"
 	NodeStatusFieldHostname        = "hostname"
 	NodeStatusFieldIPAddress       = "ipAddress"
 	NodeStatusFieldInfo            = "info"
-	NodeStatusFieldNodeConditions  = "nodeConditions"
+	NodeStatusFieldLimits          = "limits"
+	NodeStatusFieldNodeAnnotations = "nodeAnnotations"
+	NodeStatusFieldNodeLabels      = "nodeLabels"
+	NodeStatusFieldNodeName        = "nodeName"
+	NodeStatusFieldNodeTaints      = "nodeTaints"
+	NodeStatusFieldRequested       = "requested"
 	NodeStatusFieldVolumesAttached = "volumesAttached"
 	NodeStatusFieldVolumesInUse    = "volumesInUse"
 )
@@ -15,10 +21,16 @@ const (
 type NodeStatus struct {
 	Allocatable     map[string]string         `json:"allocatable,omitempty"`
 	Capacity        map[string]string         `json:"capacity,omitempty"`
+	Conditions      []NodeCondition           `json:"conditions,omitempty"`
 	Hostname        string                    `json:"hostname,omitempty"`
 	IPAddress       string                    `json:"ipAddress,omitempty"`
 	Info            *NodeInfo                 `json:"info,omitempty"`
-	NodeConditions  []NodeCondition           `json:"nodeConditions,omitempty"`
+	Limits          map[string]string         `json:"limits,omitempty"`
+	NodeAnnotations map[string]string         `json:"nodeAnnotations,omitempty"`
+	NodeLabels      map[string]string         `json:"nodeLabels,omitempty"`
+	NodeName        string                    `json:"nodeName,omitempty"`
+	NodeTaints      []Taint                   `json:"nodeTaints,omitempty"`
+	Requested       map[string]string         `json:"requested,omitempty"`
 	VolumesAttached map[string]AttachedVolume `json:"volumesAttached,omitempty"`
 	VolumesInUse    []string                  `json:"volumesInUse,omitempty"`
 }
