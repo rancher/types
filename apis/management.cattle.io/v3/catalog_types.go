10b282a43698ae5e225ae683433d8a7f635087b9
Daishan Peng <daishan@rancher.com>
2018-02-26 12:46:41 -0700

    add display name, add answer yaml, delete unused fields

--- apis/management.cattle.io/v3/catalog_types.go
+++ apis/management.cattle.io/v3/catalog_types.go
@@ -45,6 +45,7 @@ type Template struct {
 }
 
 type TemplateSpec struct {
+	DisplayName              string `json:"displayName"`
 	CatalogID                string `json:"catalogId,omitempty" norman:"type=reference[catalog]"`
 	DefaultTemplateVersionID string `json:"defaultTemplateVersionId,omitempty" norman:"type=reference[templateVersion]"`
 
