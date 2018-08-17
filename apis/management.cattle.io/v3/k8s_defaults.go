c8034b5e78557f0dbd378bb0a1f19e9e3bcb71cb
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2018-08-17 03:17:51 +0200

    Add cipher suites to k8s 1.11 and update it to 1.11.2

--- apis/management.cattle.io/v3/k8s_defaults.go
+++ apis/management.cattle.io/v3/k8s_defaults.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	DefaultK8s = "v1.11.1-rancher1-1"
+	DefaultK8s = "v1.11.2-rancher1-1"
 )
 
 var (
@@ -18,7 +18,7 @@ var (
 	k8sVersionsCurrent = []string{
 		"v1.9.7-rancher2-2",
 		"v1.10.5-rancher1-2",
-		"v1.11.1-rancher1-1",
+		"v1.11.2-rancher1-1",
 	}
 
 	// K8sVersionToRKESystemImages is dynamically populated on init() with the latest versions
@@ -26,6 +26,14 @@ var (
 
 	// K8sVersionServiceOptions - service options per k8s version
 	K8sVersionServiceOptions = map[string]KubernetesServicesOptions{
+		"v1.11": {
+			KubeAPI: map[string]string{
+				"tls-cipher-suites": "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
+			},
+			Kubelet: map[string]string{
+				"tls-cipher-suites": "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
+			},
+		},
 		"v1.10": {
 			KubeAPI: map[string]string{
 				"tls-cipher-suites":        "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
@@ -422,9 +430,9 @@ var (
 			IngressBackend:            m("k8s.gcr.io/defaultbackend:1.4"),
 			MetricsServer:             m("gcr.io/google_containers/metrics-server-amd64:v0.2.1"),
 		},
-		"v1.11.1-rancher1-1": {
+		"v1.11.2-rancher1-1": {
 			Etcd:                      m("quay.io/coreos/etcd:v3.2.18"),
-			Kubernetes:                m("rancher/hyperkube:v1.11.1-rancher1"),
+			Kubernetes:                m("rancher/hyperkube:v1.11.2-rancher1"),
 			Alpine:                    m("rancher/rke-tools:v0.1.13"),
 			NginxProxy:                m("rancher/rke-tools:v0.1.13"),
 			CertDownloader:            m("rancher/rke-tools:v0.1.13"),
