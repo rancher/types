fc1857567373fa1104ea9dee107427d9a727f2da
Craig Jellick <craig@rancher.com>
2018-08-24 14:37:17 -0700

    Revert "Supporting Windows"
    This reverts commit 537f86e63c290c629c4ffae71d80a22992c165c4.

--- apis/management.cattle.io/v3/k8s_defaults.go
+++ apis/management.cattle.io/v3/k8s_defaults.go
@@ -499,7 +499,4 @@ func init() {
 	if _, ok := K8sVersionToRKESystemImages[DefaultK8s]; !ok {
 		panic("Default K8s version " + DefaultK8s + " is not found in k8sVersionsCurrent list")
 	}
-
-	// init Windows versions
-	initWindows()
 }
