rancher/types

commit 0968d0929684772d428e67f09630e87f3fda73ba

fix regular expression
https://github.com/rancher/rancher/issues/14939.
Problems:
we are not using lazy match to match the group (!=|=) so that "foo !=
bar` can be parsed wrongly as (foo !) (=) (bar).
Solutions:
Use lazy match to match as less as possible the characters in the first
group. Also improved the whitespace matches.

mapper/scheduling.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	exprRegexp = regexp.MustCompile("^(.*)(=|!=|<|>| in | notin )(.*)$")
+	exprRegexp = regexp.MustCompile("^(.*?)\\s*(=|!=|<|>|in|notin)\\s*(.*)$")
 )
 
 type SchedulingMapper struct {

commit 5fdeb3bbcb3ac88c52af446106ee2019b239e4e3

fix regresssion for scheduling
https://github.com/rancher/rancher/issues/15379.
Fix the regression introduced by https://github.com/rancher/types/pull/535. We accidently remove the whitespace between in and notin.

mapper/scheduling.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	exprRegexp = regexp.MustCompile("^(.*?)\\s*(=|!=|<|>|in|notin)\\s*(.*)$")
+	exprRegexp = regexp.MustCompile("^(.*?)\\s*(=|!=|<|>| in | notin )\\s*(.*)$")
 )
 
 type SchedulingMapper struct {