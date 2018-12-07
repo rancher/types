960b121eb4e00474fd0d8e68e430bfca14ff9a9d
Jason Greathouse <jgreat@jgreat.me>
2018-12-07 11:51:42 -0600

    remove yaml/json tags

--- apis/management.cattle.io/v3/rke_types.go
+++ apis/management.cattle.io/v3/rke_types.go
@@ -24,7 +24,7 @@ type RancherKubernetesEngineConfig struct {
 	// Enable/disable strict docker version checking
 	IgnoreDockerVersion bool `yaml:"ignore_docker_version" json:"ignoreDockerVersion" norman:"default=true"`
 	// Is this a DinD install?
-	DinD bool `yaml:"dind" json:"dind"`
+	DinD bool
 	// Kubernetes version to use (if kubernetes image is specifed, image version takes precedence)
 	Version string `yaml:"kubernetes_version" json:"kubernetesVersion,omitempty"`
 	// List of private registries and their credentials
