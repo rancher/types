93dc9ff0d6fd125f9f252d1138ce2baab99374ad
rajashree <rajashree.28m@gmail.com>
2019-01-10 21:08:50 +0530

    RBAC for multiclusterapp and globalDNS

--- apis/management.cattle.io/v3/schema/schema.go
+++ apis/management.cattle.io/v3/schema/schema.go
@@ -620,6 +620,7 @@ func clusterCatalogTypes(schemas *types.Schemas) *types.Schemas {
 func multiClusterAppTypes(schemas *types.Schemas) *types.Schemas {
 	return schemas.
 		AddMapperForType(&Version, v3.MultiClusterApp{}, m.Drop{Field: "namespaceId"}).
+		AddMapperForType(&Version, v3.Member{}, m.Drop{Field: "userName"}, m.Drop{Field: "displayName"}).
 		MustImport(&Version, v3.MultiClusterApp{}).
 		MustImport(&Version, v3.Target{})
 }
