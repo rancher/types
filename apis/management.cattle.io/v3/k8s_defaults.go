5ba005b1442fdb926c4006d2a88098d0aab9b0a5
Alena Prokharchyk <alena@rancher.com>
2019-02-27 13:01:25 -0800

    k8s v1.12.6 support

--- apis/management.cattle.io/v3/k8s_defaults.go
+++ apis/management.cattle.io/v3/k8s_defaults.go
@@ -769,9 +769,9 @@ var (
 			IngressBackend:            m("k8s.gcr.io/defaultbackend:1.4"),
 			MetricsServer:             m("gcr.io/google_containers/metrics-server-amd64:v0.3.1"),
 		},
-		"v1.12.5-rancher1-1": {
+		"v1.12.6-rancher1-1": {
 			Etcd:                      m("quay.io/coreos/etcd:v3.2.24"),
-			Kubernetes:                m("rancher/hyperkube:v1.12.5-rancher1"),
+			Kubernetes:                m("rancher/hyperkube:v1.12.6-rancher1"),
 			Alpine:                    m("rancher/rke-tools:v0.1.16"),
 			NginxProxy:                m("rancher/rke-tools:v0.1.16"),
 			CertDownloader:            m("rancher/rke-tools:v0.1.16"),
