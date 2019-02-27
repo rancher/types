f52b54c0871aae9c1c608eb24c0a2763b7704bd3
Alena Prokharchyk <alena@rancher.com>
2019-02-27 13:01:25 -0800

    k8s v1.12.6 support

--- apis/management.cattle.io/v3/k8s_windows_default.go
+++ apis/management.cattle.io/v3/k8s_windows_default.go
@@ -262,9 +262,9 @@ var (
 			CanalCNIBinaries:   m("rancher/canal-cni:v0.0.1-nanoserver-1803"),
 			KubeletPause:       m("rancher/kubelet-pause:v0.0.1-nanoserver-1803"),
 		},
-		"v1.12.5-rancher1-1": {
+		"v1.12.6-rancher1-1": {
 			NginxProxy:         m("rancher/nginx-proxy:v0.0.1-nanoserver-1803"),
-			KubernetesBinaries: m("rancher/hyperkube:v1.12.5-nanoserver-1803"),
+			KubernetesBinaries: m("rancher/hyperkube:v1.12.6-nanoserver-1803"),
 			FlannelCNIBinaries: m("rancher/flannel-cni:v0.0.1-nanoserver-1803"),
 			CalicoCNIBinaries:  m("rancher/calico-cni:v0.0.1-nanoserver-1803"),
 			CanalCNIBinaries:   m("rancher/canal-cni:v0.0.1-nanoserver-1803"),
