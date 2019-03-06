dae6dfd3aba3683b292f5c3d8957ae012ca293c4
Alena Prokharchyk <alena@rancher.com>
2019-03-06 14:29:30 -0800

    Added k8s versions from 2.1.x release

--- apis/management.cattle.io/v3/k8s_windows_default.go
+++ apis/management.cattle.io/v3/k8s_windows_default.go
@@ -247,7 +247,7 @@ var (
 			CanalCNIBinaries:   m("rancher/canal-cni:v0.0.1-nanoserver-1803"),
 			KubeletPause:       m("rancher/kubelet-pause:v0.0.1-nanoserver-1803"),
 		},
-		"v1.13.4-rancher1-1": {
+		"v1.13.4-rancher1-2": {
 			NginxProxy:         m("rancher/nginx-proxy:v0.0.1-nanoserver-1803"),
 			KubernetesBinaries: m("rancher/hyperkube:v1.13.4-nanoserver-1803"),
 			FlannelCNIBinaries: m("rancher/flannel-cni:v0.0.1-nanoserver-1803"),
