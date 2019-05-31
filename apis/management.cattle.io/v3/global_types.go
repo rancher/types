d414535f1a160af6e9f27202811f6ddf8ce60ebc
rmweir <ricardo.weir1@gmail.com>
2019-05-31 15:25:38 -0700

    remove features

--- apis/management.cattle.io/v3/global_types.go
+++ apis/management.cattle.io/v3/global_types.go
@@ -14,16 +14,6 @@ type Setting struct {
 	Source     string `json:"source" norman:"nocreate,noupdate,options=db|default|env"`
 }
 
-type Feature struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Value      bool   `json:"value" norman:"required"`
-	Default    bool   `json:"default" norman:"nocreate,noupdate"`
-	Customized bool   `json:"customized" norman:"nocreate,noupdate"`
-	Source     string `json:"source" norman:"nocreate,noupdate,options=db|default|env"`
-}
-
 type ListenConfig struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
