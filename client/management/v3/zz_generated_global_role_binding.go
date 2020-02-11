f0034a7cc5d4ce900bbd3bf82181947a0165733c
rawmind0 <rawmind@gmail.com>
2020-02-11 17:16:45 +0100

    Updated go generate

--- client/management/v3/zz_generated_global_role_binding.go
+++ client/management/v3/zz_generated_global_role_binding.go
@@ -92,13 +92,14 @@ func (c *GlobalRoleBindingClient) ListAll(opts *types.ListOpts) (*GlobalRoleBind
 		return resp, err
 	}
 	data := resp.Data
-	for resp, err = resp.Next(); resp != nil && err == nil; resp, err = resp.Next() {
-		data = append(data, resp.Data...)
+	for next, err := resp.Next(); next != nil && err == nil; next, err = next.Next() {
+		data = append(data, next.Data...)
+		resp = next
+		resp.Data = data
 	}
 	if err != nil {
 		return resp, err
 	}
-	resp.Data = data
 	return resp, err
 }
 
--- client/project/v3/zz_generated_namespaced_basic_auth.go
+++ client/project/v3/zz_generated_namespaced_basic_auth.go
@@ -96,13 +96,14 @@ func (c *NamespacedBasicAuthClient) ListAll(opts *types.ListOpts) (*NamespacedBa
 		return resp, err
 	}
 	data := resp.Data
-	for resp, err = resp.Next(); resp != nil && err == nil; resp, err = resp.Next() {
-		data = append(data, resp.Data...)
+	for next, err := resp.Next(); next != nil && err == nil; next, err = next.Next() {
+		data = append(data, next.Data...)
+		resp = next
+		resp.Data = data
 	}
 	if err != nil {
 		return resp, err
 	}
-	resp.Data = data
 	return resp, err
 }
 
