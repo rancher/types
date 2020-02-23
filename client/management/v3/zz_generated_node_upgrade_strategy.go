08dbef56c2f88dc67baa5024b81b5dbbce350d5f
rajashree <rajashree.28m@gmail.com>
2020-02-23 15:26:15 -0800

    Generated changes - maxUnavailable controlplane

--- client/management/v3/zz_generated_node_upgrade_strategy.go
+++ client/management/v3/zz_generated_node_upgrade_strategy.go
@@ -1,14 +1,16 @@
 package client
 
 const (
-	NodeUpgradeStrategyType                = "nodeUpgradeStrategy"
-	NodeUpgradeStrategyFieldDrain          = "drain"
-	NodeUpgradeStrategyFieldDrainInput     = "nodeDrainInput"
-	NodeUpgradeStrategyFieldMaxUnavailable = "maxUnavailable"
+	NodeUpgradeStrategyType                       = "nodeUpgradeStrategy"
+	NodeUpgradeStrategyFieldDrain                 = "drain"
+	NodeUpgradeStrategyFieldDrainInput            = "nodeDrainInput"
+	NodeUpgradeStrategyFieldMaxUnavailableControl = "maxUnavailableControl"
+	NodeUpgradeStrategyFieldMaxUnavailableWorker  = "maxUnavailableWorker"
 )
 
 type NodeUpgradeStrategy struct {
-	Drain          bool            `json:"drain,omitempty" yaml:"drain,omitempty"`
-	DrainInput     *NodeDrainInput `json:"nodeDrainInput,omitempty" yaml:"nodeDrainInput,omitempty"`
-	MaxUnavailable string          `json:"maxUnavailable,omitempty" yaml:"maxUnavailable,omitempty"`
+	Drain                 bool            `json:"drain,omitempty" yaml:"drain,omitempty"`
+	DrainInput            *NodeDrainInput `json:"nodeDrainInput,omitempty" yaml:"nodeDrainInput,omitempty"`
+	MaxUnavailableControl string          `json:"maxUnavailableControl,omitempty" yaml:"maxUnavailableControl,omitempty"`
+	MaxUnavailableWorker  string          `json:"maxUnavailableWorker,omitempty" yaml:"maxUnavailableWorker,omitempty"`
 }
